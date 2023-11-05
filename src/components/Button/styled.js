77215aedb393877c85bd6dba02811a605876ae09
seogaeun <se0323333@gmail.com>
2023-11-05 18:44:46 +0900

    [ADD] 컴포넌트 스타일 추가

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,20 +1,30 @@
 import styled from 'styled-components';
 
 const ButtonStyles = {
-  btn1: {
-    width: '7vw',
-    height: '5vh',
-    borderRadius: '15px',
+  iconBtn: {
+    width: '1.25rem', //20px
+    height: '1.25rem', //20px
   },
+  mainBtn: {
+    width: '3.75rem', //60px
+    height: '1.87rem', //30px
+  },
+  quickBtn:{
+    width: '2.5rem', //20px
+    height: '2.5rem', //20px
+  },
+  lookBtn:{
+    width: '2.5rem', //20px
+    height: '2.5rem', //20px
+  }
 };
 
 export const StyledButton = styled.button`
   ${(props) => ButtonStyles[props.theme]}
   &:active, &:focus {
     transform: scale(0.95);
-  }
+  },
 
-  ,
   &:hover {
     transform: scale(1);
     cursor: pointer;
