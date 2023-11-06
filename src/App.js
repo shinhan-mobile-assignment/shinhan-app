115f766bac2e5447017cec56271841730c52ed0b
JZU0 <ljzh1030@gmail.com>
2023-11-07 01:04:20 +0900

    [ADD] 헤더, 푸터, 슬라이더 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './styles/reset.css';
-import { Text, Input } from './components';
+import { Header, Stock, Footer } from './components';
 
 function App() {
   return (
     <>
-      <Text theme="text1">안녕하세요. 빌드 테스트</Text>
-      <Input />
+      <Header />
+      <Stock />
+      <Footer />
     </>
   );
 }
